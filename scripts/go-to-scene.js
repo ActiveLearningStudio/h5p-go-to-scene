60f72fbdbd36f28dc1e2c98bc96036e13973df14
Thomas Marstrander <marstranderthomas@gmail.com>
2019-01-30 11:45:14 +0100

    HFP-2520 Remove view code, go to scene is just an empty shell

--- scripts/go-to-scene.js
+++ scripts/go-to-scene.js
@@ -1,24 +1,11 @@
 H5P.GoToScene = (function () {
 
-  function GoToScene(params) {
-    console.log("params", params);
+  function GoToScene() {
 
     /**
      * Attach interaction
-     *
-     * @param {H5P.jQuery} $container
      */
-    this.attach = function ($container) {
-      var self = this;
-      const goToScene = document.createElement('div');
-      goToScene.textContent = 'View of GoToScene content type';
-      goToScene.addEventListener('click', function () {
-        console.log("clicked goToScene with nextSceneId", params.nextSceneId);
-        self.trigger('goToScene', params.nextSceneId);
-      });
-
-      $container[0].appendChild(goToScene);
-    };
+    this.attach = function () {};
   }
 
   return GoToScene;
